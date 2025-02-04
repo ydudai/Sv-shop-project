23be141a255dc39909233aa0a96a7bcd500809dd
ydudai <ydudai@outlook.com>
2025-02-04 19:22:59 +0200

    Remove mondo data

--- server/config/db.js
+++ server/config/db.js
@@ -4,9 +4,8 @@ mongoose.connection.close();
 
 export const connectDB = () => {
     mongoose.connection.close();
-    mongoose.connect("mongodb+srv://ydudai:2tkIaFCiFXPsQYrF@cluster0.gfuz4.mongodb.net/svshop").then(() => {
-        console.log("Mongodb is connected db: svshop ...")
-        
+    mongoose.connect("mongodb+srv://<mongo-username>:<mongo-password>@cluster0.gfuz4.mongodb.net/<db-name>).then(() => {
+        //console.log("Mongodb is connected db: svshop ...");
     })
     .catch(error => {
         console.log(error)
